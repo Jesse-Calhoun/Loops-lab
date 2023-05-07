d3b9bc71c135785a2412f0eea911987e029c2533
jessejames2736@gmail.com <jessejames2736@gmail.com>
2023-05-07 01:24:08 -0400

    Created a for loop to write hello 5 times and then break using C sharp.

--- Loops-Lab/Program.cs
+++ Loops-Lab/Program.cs
@@ -3,7 +3,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello, World!");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Hello");
+        }
     }
 }
 
