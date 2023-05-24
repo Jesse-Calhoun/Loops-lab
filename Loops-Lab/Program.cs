a4b92b834fb031e227df562e169d15daf04360a3
jessejames2736@gmail.com <jessejames2736@gmail.com>
2023-05-23 20:03:17 -0400

    Completed task 3 by counting down from 10 to 0 using a for loop.

--- Loops-Lab/Program.cs
+++ Loops-Lab/Program.cs
@@ -10,7 +10,12 @@ class Program
         //    Console.WriteLine("Hello");
         //}
 
-        for (int i = 0; i < 11; i++)
+        //for (int i = 0; i < 11; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        for (int i = 10; i > -1; i--)
         {
             Console.WriteLine(i);
         }
